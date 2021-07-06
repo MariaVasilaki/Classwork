7db9c1aa2ca2f768a3b3e0baa161f105ea626879
MariaVasilaki <74157076+MariaVasilaki@users.noreply.github.com>
2021-07-06 16:42:31 +0300

    InterestCalculator with BigDecimal

--- JavaBasics/src/main/java/InterestCalculator.java
+++ JavaBasics/src/main/java/InterestCalculator.java
@@ -1,26 +1,28 @@
+import java.math.BigDecimal;
 import java.util.Scanner;
+import java.util.*;
 
 public class InterestCalculator {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("How much do you want to invest?");
-        double investment = sc.nextDouble();
+        BigDecimal investment = sc.nextBigDecimal();
         System.out.println("How many years are investing?");
         int years = sc.nextInt();
         System.out.println("What is the annual interest rate % growth?");
-        double annualRate = sc.nextDouble();
-        double quarterlyRate = annualRate/4;
-        double currentBalance = investment;
-        double earned = 0;
+        BigDecimal annualRate = sc.nextBigDecimal();
+        BigDecimal quarterlyRate = annualRate.divide(BigDecimal.valueOf(4));
+        BigDecimal currentBalance = investment;
+        BigDecimal earned;
 
         for ( int i = 1; i <= years; i++) {
             System.out.println("Year " + i + ":");
             System.out.println("Began with $" + currentBalance);
-            earned = 3* (currentBalance * (1 + (quarterlyRate / 100)));
+            earned = BigDecimal.valueOf(3).multiply((currentBalance.multiply(BigDecimal.valueOf(1).add(quarterlyRate.divide(BigDecimal.valueOf(100))))));
             System.out.println("Earned $" + earned);
 
-            System.out.println("Ended with $" + (currentBalance + earned));
-            currentBalance = currentBalance + earned;
+            System.out.println("Ended with $" + (currentBalance.add(earned)));
+            currentBalance = currentBalance.add(earned);
 
         }
     }
